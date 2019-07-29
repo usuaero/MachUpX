5a8b78637551dd4f4bf11b6944e6651ca6e64f8f
Cory <cory.goates@aggiemail.usu.edu>
2019-07-29 07:27:08 -0600

    Began implementation of interactive mode.

--- machupX/__main__.py
+++ machupX/__main__.py
@@ -6,53 +6,75 @@ from .scene import Scene
 import sys
 import json
 
-# Get input filename from command line arguments
-input_filename = sys.argv[-1]
 
-# Print sweet logo
-print()
-print('-----------------------------------------------')
-print('|                                             |')
-print('|                         BBBBB               |')
-print('|                       BB   BB               |')
-print('|                     BB     BB               |')
-print('|                    BB      BB               |')
-print('|                  BB        BB               |')
-print('|                 BB         BB               |')
-print('|               BB           BB               |')
-print('|              BB        BBBBBB               |')
-print('|                                             |')
-print('|                MachUpX 0.1                  |')
-print('|                                             |')
-print('|        (c) USU Aero Lab, LLC, 2019          |')
-print('|                                             |')
-print('|          This software comes with           |')
-print('| ABSOLUTELY NO WARRANTY EXPRESSED OR IMPLIED |')
-print('|                                             |')
-print('|           Submit bug reports to:            |')
-print('|           doug.hunsaker@usu.edu             |')
-print('-----------------------------------------------')
+def _run_interactive_mode():
+    # Runs interactive mode, allowing the user to select which analyses they want to run on the fly
+    print('Welcome to MachUpX Interactive. Type [?] for a list of available commands.')
+    
+    while True:
+        command = input(">>>")
+        
+        if command == 'q':
+            break
 
-# Initialize scene
-scene = Scene(input_filename)
+def _run_prescribed_analyses(input_filename):
+    # Runs the analyses specified in the .json file
 
-# Import JSON to determine which analyses the user wants to run
-with open(input_filename) as json_file_handle:
-    input_dict = json.load(json_file_handle)
+    # Initialize scene
+    scene = Scene(input_filename)
+    
+    # Import JSON to determine which analyses the user wants to run
+    with open(input_filename) as json_file_handle:
+        input_dict = json.load(json_file_handle)
+    
+    # Run analyses
+    for key in input_dict["run"]:
+        params = input_dict["run"].get(key, {})
+        if key == "forces":
+            filename = params.get("output_file", input_filename.replace(".json", "_forces.json"))
+            non_dimensional = params.get("non_dimensional", False)
+            verbose = params.get("verbose", False)
+    
+            print("\nCalculating aerodynamic forces...")
+            scene.solve_forces(filename=filename, non_dimensional=non_dimensional, verbose=verbose)
+    
+        elif key == "view":
+            show_legend = params.get("show_legend", False)
+    
+            print("Displaying wireframe...")
+            scene.display_wireframe(show_legend=show_legend)
 
-# Run analyses
-for key in input_dict["run"]:
-    params = input_dict["run"].get(key, {})
-    if key == "forces":
-        filename = params.get("output_file", input_filename.replace(".json", "_forces.json"))
-        non_dimensional = params.get("non_dimensional", False)
-        verbose = params.get("verbose", False)
 
-        print("\nCalculating aerodynamic forces...")
-        scene.solve_forces(filename=filename, non_dimensional=non_dimensional, verbose=verbose)
+if __name__=="__main__":
+    
+    # Print sweet logo
+    print()
+    print('-----------------------------------------------')
+    print('|                                             |')
+    print('|                         BBBBB               |')
+    print('|                       BB   BB               |')
+    print('|                     BB     BB               |')
+    print('|                    BB      BB               |')
+    print('|                  BB        BB               |')
+    print('|                 BB         BB               |')
+    print('|               BB           BB               |')
+    print('|              BB        BBBBBB               |')
+    print('|                                             |')
+    print('|                MachUpX 0.1                  |')
+    print('|                                             |')
+    print('|        (c) USU Aero Lab, LLC, 2019          |')
+    print('|                                             |')
+    print('|          This software comes with           |')
+    print('| ABSOLUTELY NO WARRANTY EXPRESSED OR IMPLIED |')
+    print('|                                             |')
+    print('|           Submit bug reports to:            |')
+    print('|           doug.hunsaker@usu.edu             |')
+    print('-----------------------------------------------')
 
-    elif key == "view":
-        show_legend = params.get("show_legend", False)
-
-        print("Displaying wireframe...")
-        scene.display_wireframe(show_legend=show_legend)
+    # Check for interactive mode
+    if "-i" in sys.argv:
+        _run_interactive_mode()
+    else:
+        # Get input filename from command line arguments
+        input_filename = sys.argv[-1]
+        _run_prescribed_analyses(input_filename)
