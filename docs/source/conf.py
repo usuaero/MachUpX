539cff14c662b934f1cadd1cdf6925dcde21e0cc
Cory <cory.goates@aggiemail.usu.edu>
2019-08-20 11:09:24 -0600

    More fixes to RTD documentation.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -54,3 +54,12 @@ html_theme = 'alabaster'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# For markdown files
+from recommonmark.parser import CommonMarkParser
+
+source_suffix = ['.rst', '.md']
+
+source_parsers= {
+    '.md' : CommonMarkParser,
+}
