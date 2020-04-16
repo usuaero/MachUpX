Fixed gradient definition for determining P0 and P1 joint locations to use displacement along LAC.

machupX/airplane.py
@@ -509,7 +509,10 @@ class Airplane:
                 #   u_j = c1*u_a+c2*T
 
                 # P0
-                T0 = np.gradient(self.P0_eff[i,wing_slice,:], self.P0_span_locs[wing_slice], edge_order=2, axis=0)
+                d_P0 = np.diff(self.P0_eff[i,wing_slice,:], axis=0)
+                ds = np.zeros(wing_slice.stop-wing_slice.start)
+                ds[1:] = np.cumsum(np.linalg.norm(d_P0, axis=1))
+                T0 = np.gradient(self.P0_eff[i,wing_slice,:], ds, edge_order=2, axis=0)
                 T0 = T0/np.linalg.norm(T0, axis=1)[:,np.newaxis]
                 u_a = self.P0_u_a[wing_slice]
                 k = np.einsum('ij,ij->i', T0, u_a)
@@ -521,7 +524,10 @@ class Airplane:
                 self.P0_joint_eff[i,wing_slice,:] = self.P0_eff[i,wing_slice,:]+c[:,np.newaxis]*delta_joint[wing_slice,np.newaxis]*u_j
 
                 # P1
-                T1 = np.gradient(self.P1_eff[i,wing_slice,:], self.P1_span_locs[wing_slice], edge_order=2, axis=0)
+                d_P1 = np.diff(self.P1_eff[i,wing_slice,:], axis=0)
+                ds = np.zeros(wing_slice.stop-wing_slice.start)
+                ds[1:] = np.cumsum(np.linalg.norm(d_P1, axis=1))
+                T1 = np.gradient(self.P1_eff[i,wing_slice,:], ds, edge_order=2, axis=0)
                 T1 = T1/np.linalg.norm(T1, axis=1)[:,np.newaxis]
                 u_a = self.P1_u_a[wing_slice]
                 k = np.einsum('ij,ij->i', T1, u_a)