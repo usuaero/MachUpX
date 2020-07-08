Various fixes to improve changes made to LQC calculation.

dev/test.py
@@ -71,9 +71,11 @@ if __name__=="__main__":
                 "side" : "both",
                 "is_main" : True,
                 "airfoil" : "NACA_0010",
-                "quarter_chord_locs" : [[-2.0, 2.0, -0.0],
-                                        [-2.0, 3.0, -0.5],
-                                        [-2.0, 4.0, -1.0]],
+                #"semispan" : np.sqrt(17),
+                #"dihedral" : np.degrees(np.arctan(0.25)),
+                #"sweep" : 45.0,
+                "quarter_chord_locs" : [[-4.12310563/2, 2.0, -0.5],
+                                        [-4.12310563, 4.0, -1.0]],
                 "control_surface" : {
                     "chord_fraction" : 0.4,
                     "root_span" : 0.55,
@@ -109,7 +111,7 @@ if __name__=="__main__":
     scene = MX.Scene(input_dict)
     scene.add_aircraft("plane", airplane_dict, state=state, control_state=control_state)
 
-    #scene.display_wireframe(show_vortices=False)
+    scene.display_wireframe(show_vortices=False)
     #scene.export_stl(filename="plane.stl")
 
     # Solve forces
@@ -117,11 +119,7 @@ if __name__=="__main__":
     print(json.dumps(FM["plane"]["total"], indent=4))
 
     # Plot lift distribution
-    dist = scene.distributions()
-    print(np.degrees(dist["plane"]["main_wing_left"]["sweep"]))
-    print(np.degrees(dist["plane"]["main_wing_right"]["sweep"]))
-    print(np.degrees(dist["plane"]["main_wing_left"]["dihedral"]))
-    print(np.degrees(dist["plane"]["main_wing_right"]["dihedral"]))
+    #dist = scene.distributions()
 
     ## Get derivatives
     #derivs = scene.derivatives(wind_frame=False)

machupX/wing_segment.py
@@ -71,7 +71,6 @@ class WingSegment:
             self._setup_cp_data()
             self._setup_node_data()
 
-
     
     def _initialize_params(self):
 
@@ -200,19 +199,19 @@ class WingSegment:
         # Sets getters for functions which are a function of span
 
         # Determine how the wing LQC has been given
-        self.b = import_value("semispan", self._input_dict, self._unit_sys, -1)
-        qc_loc_data = import_value("quarter_chord_locs", self._input_dict, self._unit_sys, -1)
-        dihedral_data = import_value("dihedral", self._input_dict, self._unit_sys, -1)
-        sweep_data = import_value("sweep", self._input_dict, self._unit_sys, -1)
+        self.b = import_value("semispan", self._input_dict, self._unit_sys, "not_given")
+        qc_loc_data = import_value("quarter_chord_locs", self._input_dict, self._unit_sys, "not_given")
+        dihedral_data = import_value("dihedral", self._input_dict, self._unit_sys, "not_given")
+        sweep_data = import_value("sweep", self._input_dict, self._unit_sys, "not_given")
 
         # Check for redundant definitions
-        if self.b == -1 and not isinstance(qc_loc_data, np.ndarray):
+        if isinstance(self.b, str) and not isinstance(qc_loc_data, np.ndarray):
             raise IOError("Either 'semispan' or 'quarter_chord_locs' must be specified.")
-        if self.b != -1 and isinstance(qc_loc_data, np.ndarray):
+        if not isinstance(self.b, str) and isinstance(qc_loc_data, np.ndarray):
             raise IOError("'semispan' and 'quarter_chord_locs' may not both be specified at once.")
-        if dihedral_data != -1 and isinstance(qc_loc_data, np.ndarray):
+        if not isinstance(dihedral_data, str) and isinstance(qc_loc_data, np.ndarray):
             raise IOError("'dihedral' and 'quarter_chord_locs' may not both be specified at once.")
-        if sweep_data != -1 and isinstance(qc_loc_data, np.ndarray):
+        if not isinstance(sweep_data, str) and isinstance(qc_loc_data, np.ndarray):
             raise IOError("'sweep' and 'quarter_chord_locs' may not both be specified at once.")
 
         # Perform various computations based on whether qc points are given
@@ -249,6 +248,12 @@ class WingSegment:
 
             # Store discontinuities to make the integrators more reliable
             self._discont = []
+
+            # Restore defaults
+            if isinstance(dihedral_data, str):
+                dihedral_data = 0.0
+            if isinstance(sweep_data, str):
+                sweep_data = 0.0
 
         # Twist
         twist_data = import_value("twist", self._input_dict, self._unit_sys, 0.0)